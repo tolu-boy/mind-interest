ace118664f3bd1c5708c45379bbe2239ed92550b
toluboy <tolualonge123@gmail.com>
2022-04-18 19:05:24 +0100

    statistics page completed

--- src/views/app-views/statistics/Statistics.js
+++ src/views/app-views/statistics/Statistics.js
@@ -1,23 +1,30 @@
 import React from "react";
 import { Card, Row, Col, Select, Avatar, Button, Table, Tag } from "antd";
-import { COLOR_1, COLOR_TEXT, COLOR_2, COLOR_4 ,COLORS, COLOR_4_LIGHT} from "constants/ChartConstant";
+import {
+  COLOR_1,
+  COLOR_TEXT,
+  COLOR_2,
+  COLOR_4,
+  COLORS,
+  COLOR_4_LIGHT,
+} from "constants/ChartConstant";
 import Chart from "react-apexcharts";
 
-import { Line } from "react-chartjs-2";
+import { Line, Doughnut } from "react-chartjs-2";
 import { ArrowUpOutlined } from "@ant-design/icons";
 import profileImg from "../../../assets/img/thumb-1.jpg";
 import left1 from "../../../assets/img/left1.svg";
 import refundImg from "../../../assets/img/refund-img.png";
-
-
-
+import phone1 from "../../../assets/img/phone1.svg";
+import phone2 from "../../../assets/img/phone2.svg";
+import avatar from "../../../assets/img/Avatar.svg";
 
 const Statistics = () => {
   const data = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
       {
-        label: "Series A",
+        label: "false",
         data: [70, 59, 80, 50, 70, 55, 100],
         fill: false,
         backgroundColor: "transparent",
@@ -36,6 +43,10 @@ const Statistics = () => {
     },
     tooltips: {
       mode: "index",
+    },
+
+    legend: {
+      display: false,
     },
     scales: {
       xAxes: [
@@ -75,171 +86,215 @@ const Statistics = () => {
     },
   };
 
+  const Chartstate = {
+    series: [
+      {
+        name: "Google",
+        data: [44, 55, 41, 67, 22, 43],
+      },
+      {
+        name: "Snapchat",
+        data: [13, 23, 20, 8, 13, 27],
+      },
 
- const Chartstate = {
-    series: [{
-        name: 'Google',
-        data: [44, 55, 41, 67, 22, 43]
-    }, {
-        name: 'Snapchat',
-        data: [13, 23, 20, 8, 13, 27]
-    },
-    
-    {
-        name: 'Word of mouth',
-        data: [11, 17, 15, 15, 21, 14]
-    },
+      {
+        name: "Word of mouth",
+        data: [11, 17, 15, 15, 21, 14],
+      },
 
-    {
-        name: 'IG or Facebook',
-        data: [11, 17, 15, 15, 21, 14]
-    },
+      {
+        name: "IG or Facebook",
+        data: [11, 17, 15, 15, 21, 14],
+      },
 
-    {
-        name: 'Other',
-        data: [11, 17, 15, 15, 21, 14]
-    }
-
-],
+      {
+        name: "Other",
+        data: [11, 17, 15, 15, 21, 14],
+      },
+    ],
     options: {
-        chart: {
-            stacked: true,
-            toolbar: {
-                show: false
+
+        dataLabels: {
+            enabled: false
+          },
+          legend: {
+            show: false
+          },
+
+      chart: {
+        stacked: true,
+        toolbar: {
+          show: false,
+        },
+        zoom: {
+          enabled: true,
+        },
+      },
+      colors: ["#3F91F5", "#FFBC99", "#B1E5FC", "#CABDFF", "#FFD88D"],
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            legend: {
+              position: "bottom",
+              offsetX: -10,
+              offsetY: 0,
             },
-            zoom: {
-                enabled: true
-            }
+          },
         },
-        colors: ['#3F91F5', '#FFBC99', '#B1E5FC' ,'#CABDFF', '#FFD88D'],
-        responsive: [{
-            breakpoint: 480,
-            options: {
-                legend: {
-                    position: 'bottom',
-                    offsetX: -10,
-                    offsetY: 0
-                }
-            }
-        }],
-        plotOptions: {
-            bar: {
-                horizontal: false,
-            },
+      ],
+      plotOptions: {
+        bar: {
+          horizontal: false,
         },
-        xaxis: {
-            categories: ['Google', 'Snapchat', 'Word of mouth', 'IG or Facebook',
-                'Other'
-            ],
-        },
-        legend: {
-            show: true,
-        position: 'bottom',
-        
-        },
-        fill: {
-            opacity: 1
-        }
-    }
-};
+      },
+      xaxis: {
+        categories: [
+          "Google",
+          "Snapchat",
+          "Word of mouth",
+          "IG or Facebook",
+          "Other",
+        ],
+      },
+      legend: {
+        show: true,
+        position: "bottom",
+      },
+      fill: {
+        opacity: 1,
+      },
+    },
+  };
 
-
-
-const data1 = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  const data1 = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
       {
-        label: 'Series A',
+        label: "Series A",
         data: [65, 59, 80, 81, 56, 55, 40],
         fill: false,
-        backgroundColor: 'transparent',
+        backgroundColor: "transparent",
         borderColor: COLOR_1,
         pointBackgroundColor: COLOR_1,
         pointHoverBackgroundColor: COLOR_1,
-        pointHoverBorderColor: COLOR_1
+        pointHoverBorderColor: COLOR_1,
       },
       {
-        label: 'Series B',
+        label: "Series B",
         data: [28, 48, 40, 19, 86, 27, 90],
         fill: false,
-        backgroundColor: 'transparent',
+        backgroundColor: "transparent",
         borderColor: COLOR_2,
         pointBackgroundColor: COLOR_2,
         pointHoverBackgroundColor: COLOR_2,
-        pointHoverBorderColor: COLOR_2
+        pointHoverBorderColor: COLOR_2,
       },
 
       {
-        label: 'Series c',
+        label: "Series c",
         data: [48, 58, 10, 29, 36, 77, 60],
         fill: false,
-        backgroundColor: 'transparent',
+        backgroundColor: "transparent",
         borderColor: COLOR_4,
         pointBackgroundColor: COLOR_4,
         pointHoverBackgroundColor: COLOR_4,
-        pointHoverBorderColor: COLOR_4_LIGHT
-      }
-    ]
+        pointHoverBorderColor: COLOR_4_LIGHT,
+      },
+    ],
   };
   const options1 = {
     responsive: true,
     hover: {
-      mode: 'nearest',
-      intersect: true
+      mode: "nearest",
+      intersect: true,
     },
     tooltips: {
-      mode: 'index'
+      mode: "index",
+    },
+
+    legend: {
+      display: false,
     },
     scales: {
-      xAxes: [{ 
-        gridLines: [{
-          display: false,
-        }],
-        ticks: {
-          display: true,
-          fontColor: COLOR_TEXT,
-          fontSize: 13,
-          padding: 10
-        }
-      }],
-      yAxes: [{
-        gridLines: {
-          drawBorder: false,
-          drawTicks: false,
-          borderDash: [3, 4],
-          zeroLineWidth: 1,
-          zeroLineBorderDash: [3, 4]  
+      xAxes: [
+        {
+          gridLines: [
+            {
+              display: false,
+            },
+          ],
+          ticks: {
+            display: true,
+            fontColor: COLOR_TEXT,
+            fontSize: 13,
+            padding: 10,
+          },
         },
-        ticks: {
-          display: true,
-          max: 100,                            
-          stepSize: 20,
-          fontColor: COLOR_TEXT,
-          fontSize: 13,
-          padding: 10
-        }  
-      }]
-    }
-  }
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            drawBorder: false,
+            drawTicks: false,
+            borderDash: [3, 4],
+            zeroLineWidth: 1,
+            zeroLineBorderDash: [3, 4],
+          },
+          ticks: {
+            display: true,
+            max: 100,
+            stepSize: 20,
+            fontColor: COLOR_TEXT,
+            fontSize: 13,
+            padding: 10,
+          },
+        },
+      ],
+    },
+  };
+
+  const data2 = {
+    labels: ["Download Sales", "In-Store Sales", "Mail Sales"],
+    datasets: [
+      {
+        data: [40, 60],
+        backgroundColor: ["#8E59FF", "#F4BF6F"],
+        pointBackgroundColor: ["#8E59FF", "#F4BF6F"],
+      },
+    ],
+  };
+
+  const options2 = {
+    responsive: true,
+
+    legend: {
+      display: false,
+    },
+  };
 
 
- const state1 = {
-    series: [44, 55],
-    options: {
-        colors: ['#8E59FF' ,'#F4BF6F'],
-        responsive: [{
-            breakpoint: 480,
-            options: {
-                chart: {
-                    width: 228
-                },
-                legend: {
-                    position: 'left'
-                }
-            }
-        }]
-    }
-};
+
+
+  const data3 = {
+    labels: ["New User", "Returning user"],
+
+    datasets: [
+      {
+        data: [30, 70],
+        backgroundColor: ["#B5E4CA", "#3F91F5"],
+        pointBackgroundColor: ["#8E59FF", "#F4BF6F"],
+      },
+    ],
+  };
+
+  const options3 = {
+    responsive: true,
+
+    legend: {
+      position: 'bottom',
+    },
+  };
 
   const { Option } = Select;
 
@@ -265,7 +320,6 @@ const data1 = {
     </div>
   );
 
-
   const Traffic = (
     <div>
       <Row>
@@ -283,8 +337,6 @@ const data1 = {
       </Row>
     </div>
   );
-
-
 
   const ActiveUsers = (
     <div>
@@ -308,7 +360,7 @@ const data1 = {
     <div>
       <h3 className="dash-heading pt-3 pb-4">Statistics</h3>
       <Row gutter={16}>
-        <Col md={16} >
+        <Col md={16}>
           <Card title={cardHeader}>
             <Row>
               <Col md={24}>
@@ -329,7 +381,9 @@ const data1 = {
               <Col md={19}>
                 <p className="">
                   Welcome
-                  <span className="top-rated-color1"> 18 new users  <br/> </span>
+                  <span className="top-rated-color1">
+                    18 new users <br />
+                  </span>
                   with a personal message 😎
                 </p>
               </Col>
@@ -338,86 +392,149 @@ const data1 = {
                 <Button> Send Message</Button>
               </Col>
 
-          
               <Col span={6} className="">
-                  <Avatar src={profileImg} />
+                <Avatar src={profileImg} />
 
-                  <br />
-                  <p className="top-rated-color1 pt-2"> Gladys</p>
-                </Col>
+                <br />
+                <p className="top-rated-color1 pt-2"> Gladys</p>
+              </Col>
 
-                <Col span={6}>
-                  <Avatar src={profileImg} />
-                  <p className="top-rated-color1 pt-2"> Gladys</p>
-                </Col>
+              <Col span={6}>
+                <Avatar src={profileImg} />
+                <p className="top-rated-color1 pt-2"> Gladys</p>
+              </Col>
 
-                <Col span={6}>
-                  <Avatar src={profileImg} />
-                  <p className="top-rated-color1 pt-2"> Gladys</p>
-                </Col>
+              <Col span={6}>
+                <Avatar src={profileImg} />
+                <p className="top-rated-color1 pt-2"> Gladys</p>
+              </Col>
 
-                <Col span={6} className="p-left2">
-                  <Avatar src={left1} />
+              <Col span={6} className="p-left2">
+                <Avatar src={left1} />
 
-                  <p className="top-rated-color1 pt-2"> View all</p>
-                </Col>
+                <p className="top-rated-color1 pt-2"> View all</p>
+              </Col>
             </Row>
           </Card>
 
-          <Card  title={Traffic}>
-
-          <Chart
-				options={Chartstate.options}
-				series={Chartstate.series}
-				type="bar"
-				height= {300}
-			/>
+          <Card title={Traffic}>
+            <Chart
+              options={Chartstate.options}
+              series={Chartstate.series}
+              type="bar"
+              height={300}
+            />
           </Card>
 
-
-
-          <Card  title={ActiveUsers}>
-
-          <Line data={data1} options={options1} />
-
+          <Card title={ActiveUsers}>
+            <Line data={data1} options={options1} />
           </Card>
-
-
         </Col>
 
         <Col md={8}>
+          <Card title="Refund requests">
+            <Row>
+              <Col span={6}>
+                <Avatar src={refundImg} />
+              </Col>
 
-        <Card title="Refund requests">
-              <Row>
-                <Col span={6}>
-                  <Avatar src={refundImg} />
-                </Col>
+              <Col span={18}>
+                <p>
+                  You have 52 open refund requests to action. This includes 8
+                  new requests. 👀
+                </p>
+              </Col>
 
-                <Col span={18}>
-                  <p>
-                    You have 52 open refund requests to action. This includes 8
-                    new requests. 👀
-                  </p>
-                </Col>
+              <Col span={24} className="pt-2 pb-3">
+                <Button block> Review refund requests </Button>
+              </Col>
+            </Row>
+          </Card>
 
-                <Col span={24} className="pt-2 pb-3">
-                  <Button block> Review refund requests </Button>
-                </Col>
-              </Row>
-            </Card>
+          <Card title="Top device">
+            <Doughnut
+              data={data2}
+              options={options2}
+              width={228}
+              height={228}
+            />
 
-            <Card title='Top device'>
+            <Row className="pt-3">
+              <Col md={12} className="pl-2">
+                <img src={phone1} alt="" />
+                <li className="pt-2">ios</li>
+                <li className="top-rated-color-1"> 40%</li>
+              </Col>
 
-            <Chart
-				options={state1.options}
-				series={state1.series}
-				height= {300}
-				type="donut"
-			/>
+              <Col md={12}>
+                <img src={phone2} alt="" />
+                <li className="pt-2">Andriod</li>
+                <li className="top-rated-color-1"> 60%</li>
+              </Col>
+            </Row>
+          </Card>
 
-            </Card>
+          <Card title="Message">
+            <Row gutter={16}>
+              <Col md={4}>
+                <img src={avatar} alt="" />
+              </Col>
+
+              <Col md={16} className="pb-5">
+                <li> Winnifred</li>
+
+                <li> Message goes here 😊</li>
+              </Col>
+
+              <Col md={4} className="pb-3">
+                <li> 30 m</li>
+              </Col>
+
+              <Col md={4}>
+                <img src={avatar} alt="" />
+              </Col>
+
+              <Col md={16} className="pb-5">
+                <li> Winnifred</li>
+
+                <li> Message goes here 😊</li>
+              </Col>
+
+              <Col md={4} className="pb-3">
+                <li> 30 m</li>
+              </Col>
+
+              <Col md={4}>
+                <img src={avatar} alt="" />
+              </Col>
+
+              <Col md={16} className="pb-5">
+                <li> Winnifred</li>
+
+                <li> Message goes here 😊</li>
+              </Col>
+
+              <Col md={4}>
+                <li> 30 m</li>
+              </Col>
+
+              <Col md={24} className="pb-3">
+                <Button block> View all message </Button>
+              </Col>
+            </Row>
+          </Card>
 
 
+          <Card title='New customer'>
+
+          <Doughnut
+              data={data3}
+              options={options3}
+              width={228}
+              height={228}
+            />
+          
+          </Card>
         </Col>
       </Row>
     </div>
